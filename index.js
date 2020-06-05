d7b566ca6c7711567b6b0e4ef1f4893639f6d706
Kevin Malakoff <kmalakoff@gmail.com>
2020-06-04 18:09:59 -0700

    Add passthrough of dist versions.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var resolveVersions = require('./lib/resolveVersions');
 
-module.exports = function nodeResolveVersions(versionExpression, options, callback) {
+module.exports = function nodeResolveVersions(versionDetails, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = {};
   }
-  options = options || {};
 
-  if (typeof callback === 'function') return resolveVersions(versionExpression, options, callback);
+  if (typeof callback === 'function') return resolveVersions(versionDetails, options || {}, callback);
   return new Promise(function (resolve, reject) {
-    nodeResolveVersions(versionExpression, options, function (err, result) {
+    nodeResolveVersions(versionDetails, options, function (err, result) {
       err ? reject(err) : resolve(result);
     });
   });
