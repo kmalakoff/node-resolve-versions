5fe51b59189f1f82a4319ae02612d3be7c65266e
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-14 12:28:17 -0800

    Update to ts-dev-stack

---
+++ src/resolveExpression.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const isArray = require('isarray');
+
+module.exports = function resolveExpression(key, semvers, options) {
+  key = key.trim();
+  if (key === 'engines') {
+    const fullPath = path.join(options.cwd || process.cwd(), 'package.json');
+    const pkg = require(fullPath);
+    if (typeof pkg.engines === 'undefined') throw new Error(`Engines not found in ${fullPath}`);
+    if (typeof pkg.engines.node === 'undefined') throw new Error(`Engines node not found in ${fullPath}`);
+    return resolveExpression(pkg.engines.node, semvers, options);
+  }
+  const version = semvers.resolve(key, options);
+  if (!version || (isArray(version) && !version.length)) throw new Error(`Unrecognized version ${key}`);
+  return isArray(version) ? version : [version];
+};
