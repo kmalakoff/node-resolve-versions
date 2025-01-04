60ee43979ce7f3e6424aa33a9ce4e9c08ae49406
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 21:57:49 -0800

    Refactor nodeResolveVersions and update build configuration

--- src/index.mjs
+++ src/index.mjs
@@ -1,28 +1,27 @@
 import NodeSemvers from 'node-semvers';
 import resolveVersions from './resolveVersions';
 
+function worker(versionDetails, options, callback) {
+  return NodeSemvers.load((err, semvers) => {
+    if (err) return callback(err);
+    try {
+      const version = resolveVersions(semvers, versionDetails, options);
+      callback(null, version);
+    } catch (err) {
+      callback(err);
+    }
+  });
+}
+
 export default function nodeResolveVersions(versionDetails, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = {};
   }
+  options = options || {};
 
-  if (typeof callback === 'function') {
-    return NodeSemvers.load((err, semvers) => {
-      if (err) return callback(err);
-      try {
-        const version = resolveVersions(semvers, versionDetails, options || {});
-        callback(null, version);
-      } catch (err) {
-        callback(err);
-      }
-    });
-  }
-  return new Promise((resolve, reject) => {
-    nodeResolveVersions(versionDetails, options, (err, result) => {
-      err ? reject(err) : resolve(result);
-    });
-  });
+  if (typeof callback === 'function') return worker(versionDetails, options, callback);
+  return new Promise((resolve, reject) => worker(versionDetails, options, (err, result) => (err ? reject(err) : resolve(result))));
 }
 
 module.exports.sync = function nodeResolveVersionsSync(versionDetails, options) {
