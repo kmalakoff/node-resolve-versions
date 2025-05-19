afef9a2c17f7632b8689414ca73d25574637037c
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 15:02:46 -0700

    Upgrade to mandatory typescript extensions

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,17 @@
 export interface VersionResultRaw {
   version: string;
+  date: string;
   files: string[];
+  npm: string;
+  v8: string;
+  uv: string;
+  zlib: string;
+  openssl: string;
+  modules: string;
+  lts: boolean;
+  security: boolean;
 }
+
 export type VersionResult = string | VersionResultRaw;
 export type VersionCallback = (error?: Error, result?: VersionResult | undefined) => undefined;
 
