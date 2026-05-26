38e421b4b3e5105c2edcb9175428400eadab540c
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:16 -0700

    Fix broken types

--- src/resolveVersions.ts
+++ src/resolveVersions.ts
@@ -21,13 +21,13 @@ export default function resolveVersions(semvers: NodeSemvers, versionDetails: Ve
   if (versionDetails === null || versionDetails === undefined) throw new Error('versionDetails missing');
   if (typeof versionDetails === 'number') versionDetails = `${versionDetails}`;
   if (typeof versionDetails === 'string') {
-    const results = [];
+    const results: (string | VersionResultRaw)[] = [];
     const expressions = versionDetails.split(',');
     for (let index = 0; index < expressions.length; index++) {
       const versions = resolveExpression(expressions[index], semvers, options);
       Array.prototype.push.apply(results, versions);
     }
-    return filterDuplicates(results.sort(sortFunction(options)));
+    return filterDuplicates(results.sort(sortFunction(options))) as string[] | VersionResultRaw[];
   }
   if (!versionDetails.version || !versionDetails.files) throw new Error(`Unrecognized version details object: ${JSON.stringify(versionDetails)}`);
   return options.path === 'raw' ? [versionDetails] : [versionDetails.version];
