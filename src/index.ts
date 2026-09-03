e2e138491ed58e7efbe3810b2c6cb31860b1828e
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-02 20:43:33 -0700

    feat: update node-semvers dependency and refactor types to use VersionRecord

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import type { VersionCallback, VersionDetails, VersionOptions, VersionResultRaw } from './types.ts';
+import type { VersionCallback, VersionDetails, VersionOptions, VersionRecord } from './types.ts';
 import worker, { sync as workerSync } from './worker.ts';
 
 export * from './types.ts';
 
 export default function nodeResolveVersions(versionDetails: VersionDetails): Promise<string[]>;
-export default function nodeResolveVersions(versionDetails: VersionDetails): Promise<VersionResultRaw[]>;
+export default function nodeResolveVersions(versionDetails: VersionDetails): Promise<VersionRecord[]>;
 export default function nodeResolveVersions(versionDetails: VersionDetails, options: VersionOptions): Promise<string[]>;
-export default function nodeResolveVersions(versionDetails: VersionDetails, options: VersionOptions): Promise<VersionResultRaw[]>;
+export default function nodeResolveVersions(versionDetails: VersionDetails, options: VersionOptions): Promise<VersionRecord[]>;
 
 export default function nodeResolveVersions(versionDetails: VersionDetails, callback: VersionCallback): void;
 export default function nodeResolveVersions(versionDetails: VersionDetails, options: VersionOptions, callback: VersionCallback): void;
 
-export default function nodeResolveVersions(versionDetails: VersionDetails, options?: VersionOptions | VersionCallback, callback?: VersionCallback): Promise<string[] | VersionResultRaw[]> | void {
+export default function nodeResolveVersions(versionDetails: VersionDetails, options?: VersionOptions | VersionCallback, callback?: VersionCallback): Promise<string[] | VersionRecord[]> | void {
   callback = typeof options === 'function' ? options : callback;
   options = typeof options === 'function' ? {} : ((options || {}) as VersionOptions);
 
   if (typeof callback === 'function') return worker(versionDetails, options, callback);
-  return new Promise((resolve, reject) => worker(versionDetails, options, (err, result) => (err ? reject(err) : resolve(result as string[] | VersionResultRaw[]))));
+  return new Promise((resolve, reject) => worker(versionDetails, options, (err, result) => (err ? reject(err) : resolve(result as string[] | VersionRecord[]))));
 }
 
-export function sync(versionDetails: VersionDetails, options?: VersionOptions): string[] | VersionResultRaw[] {
+export function sync(versionDetails: VersionDetails, options?: VersionOptions): string[] | VersionRecord[] {
   return workerSync(versionDetails, options || {});
 }
